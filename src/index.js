3526aa09a62d499ad563e9fb9c37a45240da7d44
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-10-27 13:49:32 -0500

    Create check email function

--- src/index.js
+++ src/index.js
@@ -7,9 +7,35 @@ const postcodeInput = document.getElementById("postcode");
 const passwordInput = document.getElementById("password");
 const passwordConfirmInput = document.getElementById("confirm");
 
+function checkEmail(e) {
+    const email = e.target;
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValid = regex.test(email.value);
+    console.log(isValid);
+
+    if (!isValid) {
+        emailInput.setCustomValidity("Must be a valid email (e.g. example@example.com).");
+        console.log("Not Valid")
+    }
+    else {
+        emailInput.setCustomValidity("");
+        console.log("Valid")
+    }
+    emailInput.reportValidity();
+}
+
+emailInput.addEventListener("input", checkEmail);
 
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("High five, dude!");
+    if (!form.checkValidity()) {
+        form.reportValidity();
+        console.log("Not a valid submission");
+        return;
+    }
+
+    if (form.checkValidity()) {
+        console.log("High five, dude!");
+    }
 })
